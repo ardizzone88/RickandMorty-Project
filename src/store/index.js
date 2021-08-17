ce9897ffa7328108633afc3e59797b00cb51413e
David Ardizzone <70913658+ardizzone88@users.noreply.github.com>
2021-08-17 14:27:32 -0300

    Create components List, Filters by name and tatus, solicited images of characters

--- src/store/index.js
+++ src/store/index.js
@@ -2,24 +2,44 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    characters:[],
-    charactersFilter:[],
+    characters: [],
+    charactersFilter: []
   },
   mutations: {
     setCharacters(state, payload) {
       state.characters = payload
-  },
-  setCharactersFilter(state, payload) {
-    state.charactersFilter = payload
+    },
+    setCharactersFilter(state, payload) {
+      state.charactersFilter = payload
+    }
   },
   actions: {
     async getCharacters({commit}) {
-      try{
+      try {
         const response = await fetch('https://rickandmortyapi.com/api/character')
         const data = await response.json()
-        console.log(data)
-      }catch(error){
-        console.log(error)
+        commit('setCharacters', data.results)
+        commit('setCharactersFilter', data.results)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    filterByStatus({commit, state}, status) {
+      const filter = state.characters.filter((character) => {
+        return character.status.includes(status)
+      })
+      commit('setCharactersFilter', filter)
+    },
+    filterByName({commit, state}, name) {
+      const formatName = name.toLowerCase()
+      const filter = state.characters.filter((character) => {
+        const characterName = character.name.toLowerCase()
+        if(characterName.includes(formatName)) {
+          return character
+        }
+      })
+      commit('setCharactersFilter', filter)
+    }
   },
   modules: {
   }
